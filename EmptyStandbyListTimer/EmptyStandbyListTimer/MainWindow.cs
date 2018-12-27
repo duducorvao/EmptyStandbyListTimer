a80d918840c04b18472c9266069a4301cf2bd34d
duducorvao <eduardobruno@hotmail.co.uk>
2018-12-27 14:47:55 -0300

    Created the main application window

---
+++ EmptyStandbyListTimer/EmptyStandbyListTimer/MainWindow.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EmptyStandbyListTimer
+{
+    public partial class MainWindow : Form
+    {
+
+        private float TimeInterval { get; set; }
+        private bool ShouldExecuteImmediately { get; set; }
+
+        private static System.Timers.Timer SystemTimer { get; set; }
+
+        public MainWindow()
+        {
+            this.InitializeComponent();
+        }
+
+        private void StartTimer()
+        {
+            
+        }
+
+        private void StopTimer()
+        {
+
+        }
+
+
+
+        #region Forms Events
+        private void StartTimerBtn_Click(object sender, EventArgs e)
+        {
+            this.StartTimer();
+        }
+
+        private void StopTimerBtn_Click(object sender, EventArgs e)
+        {
+            this.StopTimer();
+        }
+        #endregion
+    }
+}
